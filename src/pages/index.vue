e748c649de544feab79422fc2875b59e8b12b26d
曹晶晶 <2550815215@qq.com>
2017-03-14 16:59:57 +0800

    my first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+	    <SwiperComponent :articleData = "articleData" v-if="articleData"></SwiperComponent>
+	    <CategoryComponent></CategoryComponent>
+    </div>
+</template>
+<style>
+
+</style>
+<script>
+	import SwiperComponent from '../components/swiper.vue'
+    import CategoryComponent from '../components/category.vue'
+    import utilities from 'utilities'
+    export default{
+        data(){
+            return{
+				showSwiper:false,
+				articleData:undefined
+            }
+        },
+        mounted(){
+            this.articleData = [
+				{
+					src:'static/timg1.jpg',
+				},
+				{
+					src:'static/timg2.jpg',
+				},
+				{
+					src:'static/timg3.jpg',
+				}
+			];
+			this.articleData.forEach(function(item){
+				item.style = {'background-image': 'url(' + item.src + ')', 'height': window.innerWidth * 0.5 + 'px'}
+			})
+			this.showSwiper = true;
+        },
+        components:{
+			SwiperComponent,
+            CategoryComponent
+        }
+    }
+</script>
